d244beeb4409e7d15583bf9633d7dae26540a796
eva27182 <koyo.0337@gmail.com>
2024-07-20 19:21:28 +0700

    render.yaml追加

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import random
+import os
 
 app = Flask(__name__)
 
@@ -120,4 +121,5 @@ def recieve_data():
     return jsonify({"status": "success", "data": main(members, weeks, groups_per_week)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
