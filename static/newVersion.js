3c8d366ef0f7738cc065f0b366131a8ad0bea1c9
unicast <152622153+eva27182@users.noreply.github.com>
2025-02-09 17:56:37 +0900

    結果表示を作成すること

--- static/newVersion.js
+++ static/newVersion.js
@@ -1,3 +1,42 @@
+// ページが読み込まれたときに状態を復元
+window.addEventListener('load', loadFormState);
+
+// フォームが変更されたときに状態を保存
+document.querySelector("form").addEventListener('input', saveFormState);
+
+function loadFormState() {
+    let formData = localStorage.getItem('formData');
+    if (formData) {
+        formData = JSON.parse(formData);
+        let inputTags = document.querySelectorAll("#name-card-container input");
+        console.log("formData:", formData);
+        let keys = Object.keys(formData)
+        console.log("formData.length:", keys.length);
+        //ボタンから追加した分のinputタグを追加
+        for (let i = 0; i < (keys.length - inputTags.length); i++) {
+            addNameArea();
+        }
+        //inputTagsの更新
+        inputTags = document.querySelectorAll("#name-card-container input");
+        inputTags.forEach((input, index) => {
+            if (formData[`input${index}`]) {
+                input.value = formData[`input${index}`];
+            }
+        });
+    }
+}
+
+//読み込んだときにデータを保存するように
+function saveFormState() {
+    let inputTags = document.querySelectorAll("#name-card-container input");
+    let formData = {};
+    inputTags.forEach((input, index) => {
+        formData[`input${index}`] = input.value;
+    });
+    localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+
 function increase() {
     let counter = document.getElementById('counter');
     counter.value = parseInt(counter.value) + 1;
@@ -96,3 +135,60 @@ function scrollToRight() {
         behavior: 'smooth'   // スムーズスクロール
     });
 }
+
+function arrayShuffle(array) {
+    for (let i = (array.length - 1); 0 < i; i--) {
+
+        // 0〜(i+1)の範囲で値を取得
+        let r = Math.floor(Math.random() * (i + 1));
+
+        // 要素の並び替えを実行
+        let tmp = array[i];
+        array[i] = array[r];
+        array[r] = tmp;
+    }
+    return array;
+}
+
+function submitData() {
+    let inputTags = document.querySelector("form").querySelectorAll("input");
+    console.log(inputTags)
+    let values = { "members": [] };
+    inputTags.forEach(elem => {
+        let name = elem.getAttribute("name");
+        if (name == "groups_per_week") {
+            values["groups_per_week"] = Number(elem.value);
+        }
+        else {
+            values["members"].push(elem.value);
+        }
+
+    })
+    values["members"] = values["members"].filter(value => value != "")
+    console.log(values["groups_per_week"], values["members"].length)
+    let tmp = [...new Set(values["members"])]; /*名前の重複判断用 */
+    //コート数が多すぎる場合、エラーを表示
+
+    values["members"] = values["members"].filter(value => value != "")
+    values["members"] = arrayShuffle(values["members"])
+    console.log(values)
+    
+    fetch('/post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+    })
+        .then(response => response.json())
+        .then(data => {
+            resetResult();
+            showGroups(data);
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+}
+
